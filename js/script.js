d51cb3afe1485ac34fcff2315e931e43da72428e
swathikrishnaus <swathikrishnaus2015@gmail.com>
2023-02-18 18:16:21 +0530

    added

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).scroll(function () {
+    var scroll = $(this).scrollTop();
+    if (scroll > 50)
+        $(".navbar").addClass("nav-shadow")
+    else
+        $(".navbar").removeClass("nav-shadow")
+})
+
+
+
+
